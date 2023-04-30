7d3adf2d92f4dd331246d516560168768e145d6d
fabian <famabiker@gmail.com>
2023-04-30 18:04:07 +0200

    add landingpage component

---
+++ src/Components/Sections/LandingPage/LandingPage.tsx
@@ -0,0 +1,8 @@
+import { BaseParameter } from "../../../contracts/Parameter";
+import "./LandingPage.scss";
+
+const LandingPage = ({ className }: BaseParameter) => {
+  return <section className={className}></section>;
+};
+
+export default LandingPage;
