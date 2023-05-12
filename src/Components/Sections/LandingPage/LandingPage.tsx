7a4f7043c168113b8461dadd2671048063047431
fabian <famabiker@gmail.com>
2023-05-12 12:05:09 +0200

    apply responsive design for tablets

--- src/Components/Sections/LandingPage/LandingPage.tsx
+++ src/Components/Sections/LandingPage/LandingPage.tsx
@@ -18,31 +18,27 @@ const LandingPage = ({ className }: BaseParameter) => {
 
   return (
     <section className={className + " " + "landing-page"}>
-      <div className="content">
-        <div className="name-header">
-          <MainHeader className="wellcome-name">
-            Hey, I&apos;m <br /> Fabian Dasler
-          </MainHeader>
-          <SecondaryHeader className="job-title">
-            Fullstack Developer
-          </SecondaryHeader>
-        </div>
-        <article>
-          I am a full stack developer with a focus on C# and the .Net Core
-          Framework. My passion is to develop solutions in the form of code. I
-          also like to get excited about innovations and new things. Improving
-          existing code or designs or use new technologies is awesome. From
-          design to architecture planning to implementation in both the front
-          and backend, I live for this job.
-        </article>
-        <div className="buttons">
-          <FilledButton onClick={handleNavigateToContact}>
-            Get in touch!
-          </FilledButton>
-          <OutlinedButton onClick={handleDonwloadCV}>
-            Download CV
-          </OutlinedButton>
-        </div>
+      <div className="name-header">
+        <MainHeader className="wellcome-name">
+          Hey, I&apos;m <br /> Fabian Dasler
+        </MainHeader>
+        <SecondaryHeader className="job-title">
+          Fullstack Developer
+        </SecondaryHeader>
+      </div>
+      <article>
+        I am a full stack developer with a focus on C# and the .Net Core
+        Framework. My passion is to develop solutions in the form of code. I
+        also like to get excited about innovations and new things. Improving
+        existing code or designs or use new technologies is awesome. From design
+        to architecture planning to implementation in both the front and
+        backend, I live for this job.
+      </article>
+      <div className="buttons">
+        <FilledButton onClick={handleNavigateToContact}>
+          Get in touch!
+        </FilledButton>
+        <OutlinedButton onClick={handleDonwloadCV}>Download CV</OutlinedButton>
       </div>
       <ProfileImage className="profile" />
     </section>
