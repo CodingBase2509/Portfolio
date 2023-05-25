c633e05a43a6283667f050a05b02ee805d7d20f2
fabian <famabiker@gmail.com>
2023-05-25 17:23:24 +0200

    add service section

---
+++ src/Components/Sections/Services/Services.tsx
@@ -0,0 +1,8 @@
+import "./Services.scss";
+import { BaseParameter } from "../../../contracts/Parameter";
+
+const Services = ({ className }: BaseParameter) => {
+  return <></>;
+};
+
+export default Services;
