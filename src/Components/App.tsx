a99f704098052292b0df487467d324adb0f48673
Fabian <famabiker@gmail.com>
2023-06-17 19:55:57 +0200

    base implementation

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,6 +3,8 @@ import './App.scss';
 import Header from './Header/Header';
 import LandingPage from './LandingPage/LandingPage';
 import Services from './Services/Services';
+import Resume from './Resume/Resume';
+import SectionLine from './Shared/SectionLine';
 import Skills from './Skills/Skills';
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
         <LandingPage className="app-section" />
         <Services className="app-section" />
         <Skills className="app-section" />
+        <SectionLine />
+        <Resume className="app-section" />
       </main>
     </>
   );
--- src/Components/Resume/Resume.tsx
+++ src/Components/Resume/Resume.tsx
@@ -1,7 +1,14 @@
+import { BaseParameter } from '../../Contracts/Parameter';
+import Section from '../Shared/Section';
 import './Resume.scss';
 
-const Resume = () => {
-  return <section></section>;
+const Resume = ({ className }: BaseParameter) => {
+  return (
+    <Section className={className + ' resume'} mainHeader="About me" secondaryHeader="Resume">
+      <div className="work-experience"></div>
+      <div className="education"></div>
+    </Section>
+  );
 };
 
 export default Resume;
